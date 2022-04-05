c8c9cee924998aa971c787c80571bb2f1318fb95
Yoel <yoelzerbib7@gmail.com>
2022-04-05 22:16:59 +0300

    added ownable, counter, and token uri support

--- contracts/HHNFT.sol
+++ contracts/HHNFT.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HHNFT is ERC721 {
+contract HHNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public totalSupply;
+    string baseUri;
+    mapping(uint256 => string) private _uris;
 
     constructor() ERC721("HHNFT", "HNFT") {}
 
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = totalSupply.current();
+        totalSupply.increment();
+        setTokenURI(tokenId, uri);
+        _safeMint(to, tokenId);
+    }
+
+    function setTokenURI(uint256 _id, string memory _uri) public onlyOwner {
+        _uris[_id] = string(abi.encodePacked(_uri, ".json"));
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
   networks: {
     rinkeby: {
       url: "https://speedy-nodes-nyc.moralis.io/fb1841515bcc2b1e0aed0509/eth/rinkeby",
